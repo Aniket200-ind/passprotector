275a20ac3f86273ef37510819d23708dbf8f70e9
Aniket <aniketbotre007@gmail.com>
2025-02-16 16:52:05 +0530

    style: format code for consistency in auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -3,11 +3,8 @@ import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./lib/prisma";
 
-
-
-
 export const { handlers, signIn, signOut, auth } = Nextauth({
-    adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
   trustHost: true,
   secret: process.env.AUTH_SECRET,
