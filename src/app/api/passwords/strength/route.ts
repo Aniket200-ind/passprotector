f23f2f1badab61c9a4c81e62ffc4cc1240d64a9b
Aniket <aniketbotre007@gmail.com>
2025-02-18 00:56:03 +0530

    feat: implement rate limiting for password strength API to enhance security and prevent abuse

--- src/app/api/passwords/strength/route.ts
+++ src/app/api/passwords/strength/route.ts
@@ -2,6 +2,27 @@
 
 import { evaluatePasswordStrength } from "@/lib/passwords/strength";
 import { NextRequest, NextResponse } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import { RatelimitResponse } from "@/lib/types/ratelimit";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Initialize the Upstash redis client
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+// Rate limit setup (20 requests per minute per IP)
+const ratelimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.slidingWindow(20, "60s"),
+  prefix: "upstash/ratelimit",
+  analytics: true,
+  timeout: 60,
+})
 
 /**
  * API route to analyze password strength.
@@ -14,6 +35,32 @@ import { NextRequest, NextResponse } from "next/server";
  */
 export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
+    // **Rate limit check**
+
+    // Extract IP address from request headers
+    const ip =
+      req.headers.get("x-real-ip") ||
+      req.headers.get("x-forwarded-for")?.split(",")[0] ||
+      "unknown";
+
+      // Apply rate limiting
+      const { success, limit, remaining, reset }: RatelimitResponse = await ratelimit.limit(ip);
+
+      // Log rate limit status
+      console.log("Rate limiter executed in /api/passwords/strength");
+      console.log(
+        `Max requests: ${limit.toString()}, Remaining: ${remaining.toString()}, Reset: ${reset.toString()}`
+      );
+
+      // If the request is over the limit, return a 429 status code
+      if (!success) {
+        return NextResponse.json(
+          { message: "Rate limit exceeded. Try again later" },
+          { status: 429 }
+        );
+      }
+
+
     // **Extract password from request body**
     const { password } = await req.json();
 
