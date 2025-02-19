2b5d71422d3b746e7794eaed3df3b908b9962bcb
Aniket <aniketbotre007@gmail.com>
2025-02-19 20:31:42 +0530

    feat: add security headers to middleware for enhanced API response protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -40,6 +40,8 @@ const ratelimit = new Ratelimit({
  * @returns {Promise<NextResponse>} - The response object, either allowing the request to proceed or indicating rate limit exceeded.
  */
 export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
   // Extract IP address from request headers
   const ip =
     req.headers.get("x-real-ip") ||
@@ -71,7 +73,25 @@ export async function middleware(req: NextRequest) {
     );
   }
 
-  return NextResponse.next();
+  //! Security headers
+  res.headers.set("X-DNS-Prefetch-Control", "off"); // Disable DNS prefetching for browsers
+  res.headers.set("X-Content-Type-Options", "nosniff"); // Prevent browsers from MIME-sniffing a response away from the declared content-type
+  res.headers.set("Referrer-Policy", "no-referrer"); // Prevent browsers from sending the Referer header with requests initiated by third party websites
+  res.headers.set(
+    "Permissions-Policy",
+    "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()"
+  ); // Restrict access to certain browser features
+  res.headers.set("X-Frame-Options", "DENY"); // Prevent clickjacking attacks by ensuring that a browser can't render a page in a frame
+  res.headers.set("X-XSS-Protection", "1; mode=block"); // Enable the Cross-site scripting (XSS) filter built into most browsers
+  res.headers.set("Cache-Control", "no-store, max-age=0"); // Prevent caching of sensitive data
+  res.headers.set("Cross-Origin-Opener-Policy", "same-origin"); // Prevent the browser from opening a page in a different origin
+  res.headers.set("Cross-Origin-Embedder-Policy", "require-corp"); // Prevent the browser from embedding a page in a different origin
+  res.headers.set("Cross-Origin-Resource-Policy", "same-origin"); // Prevent the browser from loading resources from a different origin
+  
+  // TODO: Uncomment the following line to enforce the use of HTTPS during production
+  // res.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload"); // Enforce the use of HTTPS
+
+  return res;
 }
 
 export const config = {
