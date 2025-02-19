b1db3567bd927a4593c08b9050a642993ea8643d
Aniket <aniketbotre007@gmail.com>
2025-02-20 01:45:27 +0530

    feat: enhance passphrase generation API with logging and performance metrics

--- src/app/api/generate/passphrase/route.ts
+++ src/app/api/generate/passphrase/route.ts
@@ -4,12 +4,16 @@ import { generatePassphrase } from "@/lib/passwords/generator";
 import { PassphraseGeneratorSchema } from "@/lib/validation/generatorSchema";
 import { NextRequest, NextResponse } from "next/server";
 import { applySecurityHeaders } from "@/lib/middleware/securityHeaders";
+import logger from "@/lib/logger";
+
 /**
  * @param {NextRequest} req - The incoming request object.
  * @returns {Promise<NextResponse>} res - The outgoing response object.
  */
 export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
+    const startTime = Date.now(); // Start time for measuring request duration
+
     // Parse request body
     const body = await req.json();
     const parsedData = PassphraseGeneratorSchema.safeParse(body);
@@ -52,9 +56,20 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
 
     // **Return final response**
     const res = NextResponse.json({ success: true, passphrase }, { status: 200 });
+
+    // Log request
+    logger.info({
+      timestamp: new Date().toISOString(),
+      method: req.method,
+      route: "/api/generate/passphrase",
+      ip: req.headers.get("x-real-ip") || req.headers.get("x-forwaded-for")?.split(",")[0] || "unknown",
+      status: 200,
+      responseTime: `${Date.now() - startTime}ms`,
+    })
+
     return await applySecurityHeaders(res);
   } catch (error) {
-    console.log("[ERROR] Failed to generate passphrase: ", error);
+    logger.error("[ERROR] Failed to generate passphrase: ", error);
 
     const res = NextResponse.json(
       { succes: false, message: "Internal Server Error" },
