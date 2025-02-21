67fd39f7f529613fa31854d71a05d919ba56faa6
Aniket <aniketbotre007@gmail.com>
2025-02-22 00:05:04 +0530

    chore: improve comments in middleware.ts for clarity and consistency

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import type { NextRequest } from "next/server";
 import { ratelimit } from "./lib/ratelimitConfig";
 
 /**
- * Middleware function to handle rate limiting for incoming requests.
+ *? Middleware function to handle rate limiting for incoming requests.
  *
  * @param {NextRequest} req - The incoming request object.
  * @returns {Promise<NextResponse>} - The response object, either allowing the request to proceed or indicating rate limit exceeded.
@@ -13,30 +13,30 @@ import { ratelimit } from "./lib/ratelimitConfig";
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
 
-  // Extract IP address from request headers
+  //* Extract IP address from request headers
   const ip =
     req.headers.get("x-real-ip") ||
     req.headers.get("x-forwarded-for")?.split(",")[0] ||
     "unknown";
 
-  // Apply rate limiting
+  //* Apply rate limiting
   const { success, pending, limit, remaining, reset } = await ratelimit.limit(
     ip
   );
 
-  // Log rate
+  //* Log rate
   await pending;
 
   const date = new Date(reset);
   const timeUntilReset = Math.floor((date.getTime() - Date.now()) / 1000);
 
-  // Log rate limit status
+  //* Log rate limit status
   console.log(`Rate limiter executed in ${req.nextUrl.pathname}`);
   console.log(
     `Max requests: ${limit.toString()}, Remaining: ${remaining.toString()}, Reset: ${timeUntilReset.toString()} seconds`
   );
 
-  // If the request is over the limit, return a 429 status code
+  //* If the request is over the limit, return a 429 status code
   if (!success) {
     return new NextResponse(
       JSON.stringify({ message: "Rate limit exceeded. Try again later" }),
@@ -45,22 +45,22 @@ export async function middleware(req: NextRequest) {
   }
 
   //! Security headers
-  res.headers.set("X-DNS-Prefetch-Control", "off"); // Disable DNS prefetching for browsers
-  res.headers.set("X-Content-Type-Options", "nosniff"); // Prevent browsers from MIME-sniffing a response away from the declared content-type
-  res.headers.set("Referrer-Policy", "no-referrer"); // Prevent browsers from sending the Referer header with requests initiated by third party websites
+  res.headers.set("X-DNS-Prefetch-Control", "off"); //* Disable DNS prefetching for browsers
+  res.headers.set("X-Content-Type-Options", "nosniff"); //* Prevent browsers from MIME-sniffing a response away from the declared content-type
+  res.headers.set("Referrer-Policy", "no-referrer"); //* Prevent browsers from sending the Referer header with requests initiated by third party websites
   res.headers.set(
     "Permissions-Policy",
     "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()"
-  ); // Restrict access to certain browser features
-  res.headers.set("X-Frame-Options", "DENY"); // Prevent clickjacking attacks by ensuring that a browser can't render a page in a frame
-  res.headers.set("X-XSS-Protection", "1; mode=block"); // Enable the Cross-site scripting (XSS) filter built into most browsers
-  res.headers.set("Cache-Control", "no-store, max-age=0"); // Prevent caching of sensitive data
-  res.headers.set("Cross-Origin-Opener-Policy", "same-origin"); // Prevent the browser from opening a page in a different origin
-  res.headers.set("Cross-Origin-Embedder-Policy", "require-corp"); // Prevent the browser from embedding a page in a different origin
-  res.headers.set("Cross-Origin-Resource-Policy", "same-origin"); // Prevent the browser from loading resources from a different origin
-  
+  ); //* Restrict access to certain browser features
+  res.headers.set("X-Frame-Options", "DENY"); //* Prevent clickjacking attacks by ensuring that a browser can't render a page in a frame
+  res.headers.set("X-XSS-Protection", "1; mode=block"); //* Enable the Cross-site scripting (XSS) filter built into most browsers
+  res.headers.set("Cache-Control", "no-store, max-age=0"); //* Prevent caching of sensitive data
+  res.headers.set("Cross-Origin-Opener-Policy", "same-origin"); //* Prevent the browser from opening a page in a different origin
+  res.headers.set("Cross-Origin-Embedder-Policy", "require-corp"); //* Prevent the browser from embedding a page in a different origin
+  res.headers.set("Cross-Origin-Resource-Policy", "same-origin"); //* Prevent the browser from loading resources from a different origin
+
   // TODO: Uncomment the following line to enforce the use of HTTPS during production
-  // res.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload"); // Enforce the use of HTTPS
+  // res.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload"); //* Enforce the use of HTTPS
 
   return res;
 }
