96c123cf525e24a893b922efb6e68cfb4dc9874d
Aniket <aniketbotre007@gmail.com>
2025-02-22 00:11:09 +0530

    chore: enhance comments in validation.ts for better clarity and consistency

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,29 +1,35 @@
-// lib/utils/validation.ts
+//! lib/utils/validation.ts
 import { PasswordCategory, PasswordStrength } from "@prisma/client";
 import { z } from "zod";
 
-// Schema for creating a new password entry
+//* Schema for creating a new password entry
 export const PasswordCreateSchema = z.object({
   siteName: z.string().min(3, "Site name must be at least 3 characters long"),
   siteUrl: z.string().url(),
-  password: z.string().min(8, "Password must be at least 8 characters").max(64, "Password too long"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(64, "Password too long"),
   category: z.nativeEnum(PasswordCategory).optional(),
   strength: z.nativeEnum(PasswordStrength).optional(),
   hashedPassword: z.string().optional(),
 });
 
-// Schema for updating an existing password entry
+//* Schema for updating an existing password entry
 export const PasswordUpdateSchema = z.object({
   siteName: z.string().min(1, "Site name is required").optional(),
   siteUrl: z.string().url("Invalid URL").optional(),
   category: z.string().optional(),
   strength: z.enum(["Weak", "Medium", "Strong"]).optional(),
-  password: z.string().min(8, "Password must be at least 8 characters").optional(),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .optional(),
   encryptedPassword: z.string().optional(),
   iv: z.string().optional(),
   authTag: z.string().optional(),
   hashedPassword: z.string().optional(),
 });
 
-  // Type inference from Zod
-  export type PasswordCreateInput = z.infer<typeof PasswordCreateSchema>;
+//* Type inference from Zod
+export type PasswordCreateInput = z.infer<typeof PasswordCreateSchema>;
