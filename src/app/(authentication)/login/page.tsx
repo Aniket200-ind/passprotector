07ac7b42552e2ea98075e7f7d8d8107ce59e3e8b
Aniket <aniketbotre007@gmail.com>
2025-02-24 23:35:18 +0530

    feat: create LoginPage component with Google oauth sign-in functionality

---
+++ src/app/(authentication)/login/page.tsx
@@ -0,0 +1,47 @@
+//! src/app/(authentication)/login/page.tsx
+
+import LoginButton from "@/components/ui/Button/LoginButton";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { LockIcon } from "lucide-react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login | PassProtector",
+  description: "Secure your digital life with PassProtector",
+};
+
+export default function LoginPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-background p-4 sm:p-8">
+      <section>
+        <Card className="w-full max-w-md space-y-6 overflow-hidden transition-all duration-300 shadow-2xl shadow-primary/50">
+          <CardHeader className="space-y-1 text-center">
+            <div className="flex justify-center mb-4">
+              <LockIcon className="h-14 w-14 text-golden animate-pulse" />
+            </div>
+            <CardTitle className="text-3xl font-fancy font-bold tracking-tight">
+              Welcome 👋
+            </CardTitle>
+            <CardDescription className="text-muted-foreground">
+              Sign in securely using your Google account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+                <LoginButton />
+          </CardContent>
+          <CardFooter className="text-center text-sm text-muted-foreground flex-1 underline underline-offset-2">
+            By signing in, you trust us with your data. We take this trust very
+            seriously and will never misuse your data.
+          </CardFooter>
+        </Card>
+      </section>
+    </main>
+  );
+}
