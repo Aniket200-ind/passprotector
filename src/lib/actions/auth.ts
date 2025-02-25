1574a7b5bafb7b236299f136cd63bc6bf8286c2a
Aniket <aniketbotre007@gmail.com>
2025-02-25 21:17:38 +0530

    feat: enhance login function to handle errors and redirect to error page

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -1,10 +1,13 @@
 "use server";
 
 import { signIn, signOut } from "@/auth";
+import { redirect } from "next/navigation";
 
 export const login = async () => {
-    await signIn("google", {redirectTo: "/dashboard"})
-}
+    await signIn("google", { redirectTo: "/dashboard" }).catch(() => {
+      redirect("/login/error?message=Login%20failed");
+    });
+  };
 
 export const logout = async () => {
     await signOut({redirectTo: "/"})
