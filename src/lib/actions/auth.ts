f2a0c7aa83b6cdd524d807e69a33baeb05640483
Aniket <aniketbotre007@gmail.com>
2025-02-27 02:03:12 +0530

    refactor: simplify login function by removing error handling and redirect

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -1,12 +1,9 @@
 "use server";
 
 import { signIn, signOut } from "@/auth";
-import { redirect } from "next/navigation";
 
 export const login = async () => {
-    await signIn("google", { redirectTo: "/dashboard" }).catch(() => {
-      redirect("/login/error?message=Login%20failed");
-    });
+    await signIn("google", { redirectTo: "/dashboard" })
   };
 
 export const logout = async () => {
