861b4455647b1e34a74073a33247d36f7a7e766f
Aniket <aniketbotre007@gmail.com>
2025-02-28 00:08:08 +0530

    feat: integrate ToggleGenerator and enhance layout for PasswordGeneratorPage

--- src/app/(generation)/generate/password/page.tsx
+++ src/app/(generation)/generate/password/page.tsx
@@ -1,10 +1,19 @@
 //! src/app/(generation)/generate/password/page.tsx
 
 import { PasswordGenerator } from "@/components/features/PasswordGenerator";
+import ToggleGenerator from "@/components/features/ToggleGenerator";
 
 export default function PasswordGeneratorPage() {
   return (
-    <main className="mx-auto p-4 min-h-screen flex items-center justify-center">
+    <main className="mx-auto py-12 px-4">
+      <ToggleGenerator />
+      <h1 className="opacity-0 animate-fade-in-up text-3xl motion-reduce:animate-none sm:text-4xl font-bold text-golden font-fancy text-center mb-4">
+        Generate Secure Passwords
+      </h1>
+      <p className="text-center text-muted-foreground mb-8 max-w-2xl mx-auto">
+        Secure your accounts with strong, randomly generated passwords.
+        Customize your password settings below to fit your needs.
+      </p>
       <PasswordGenerator />
     </main>
   );
