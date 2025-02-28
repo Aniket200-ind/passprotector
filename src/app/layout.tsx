222306f7b607da854f294b37120ad6313fc5c998
Aniket <aniketbotre007@gmail.com>
2025-03-01 01:16:37 +0530

    feat: integrate NavbarWrapper and session management in RootLayout for improved navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Inter, JetBrains_Mono, Montserrat } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import ThemeProvider from "@/components/theme-provider";
+import { auth } from "@/auth";
+import { NavbarWrapper } from "@/components/layout/NavBarWrapper";
 
 //* Load fonts with subsets and CSS variables
 const inter = Inter({
@@ -25,14 +27,24 @@ const montserrat = Montserrat({
 
 export const metadata: Metadata = {
   title: "PassProtector | Secure Your Passwords Effortlessly",
-  description: "PassProtector is a modern and secure password manager that helps you generate, store, and manage strong passwords effortlessly. Enhance your online security with encryption, passphrase generation, password generation, and real-time strength analysis.",
+  description:
+    "PassProtector is a modern and secure password manager that helps you generate, store, and manage strong passwords effortlessly. Enhance your online security with encryption, passphrase generation, password generation, and real-time strength analysis.",
   category: "password manager web app",
   applicationName: "PassProtector",
-  keywords: ["password manager", "password generator", "password strength", "encryption", "passphrase generator", "security", "online security"],
+  keywords: [
+    "password manager",
+    "password generator",
+    "password strength",
+    "encryption",
+    "passphrase generator",
+    "security",
+    "online security",
+  ],
   robots: "index, follow",
   openGraph: {
     title: "PassProtector | Secure Your Passwords Effortlessly",
-    description: "PassProtector is a modern and secure password manager that helps you generate, store, and manage strong passwords effortlessly. Enhance your online security with encryption, passphrase generation, password generation, and real-time strength analysis.",
+    description:
+      "PassProtector is a modern and secure password manager that helps you generate, store, and manage strong passwords effortlessly. Enhance your online security with encryption, passphrase generation, password generation, and real-time strength analysis.",
     type: "website",
     // url: "https://passprotector.vercel.app", During production deployment, uncomment this line and replace the URL with the actual domain.
     locale: "en_US",
@@ -40,18 +52,25 @@ export const metadata: Metadata = {
   publisher: "Aniket Botre",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+
   return (
     <html lang="en" className="dark">
       <body
         className={`${inter.variable} ${jetbrainsMono.variable} ${montserrat.variable} antialiased bg-charcoal dark`}
       >
         <SessionProvider>
-          <ThemeProvider>{children}</ThemeProvider>
+          <ThemeProvider>
+            <main>
+              <NavbarWrapper session={session} />
+              {children}
+            </main>
+          </ThemeProvider>
         </SessionProvider>
       </body>
     </html>
