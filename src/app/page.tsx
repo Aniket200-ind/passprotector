83f0151a104214cf521ad9b451ea7e2c6b059f20
Aniket <aniketbotre007@gmail.com>
2025-03-03 18:39:17 +0530

    feat: add HowItWorks, PasswordStrengthChecker, and SecurityTerminal components to main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,19 @@
 import WhyUseSection from "@/components/layout/EducationSection";
 import FeaturesShowcase from "@/components/layout/FeatureSection";
 import HeroSection from "@/components/layout/HeroSection";
+import HowItWorks from "@/components/layout/HowItWorks";
+import PasswordStrengthChecker from "@/components/layout/PasswordStrengthChecker";
+import SecurityTerminal from "@/components/layout/SecurityTerminalSection";
 
 export default function Page() {
   return (
-    <main className="flex flex-col items-center justify-center bg-background text-foreground mt-6">
+    <main className="flex flex-col items-center justify-center bg-background text-foreground mt-10">
       <HeroSection />
       <WhyUseSection />
+      <HowItWorks />
       <FeaturesShowcase />
+      <PasswordStrengthChecker />
+      <SecurityTerminal />
     </main>
   );
 }
