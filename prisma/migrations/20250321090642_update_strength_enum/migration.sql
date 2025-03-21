40206cbd583bb09ccb2ac8945e65541245b826d7
Aniket <aniketbotre007@gmail.com>
2025-03-21 14:39:28 +0530

    fix: update PasswordStrength enum to remove deprecated value and improve casing

---
+++ prisma/migrations/20250321090642_update_strength_enum/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [VULNERABLE] on the enum `PasswordStrength` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "PasswordStrength_new" AS ENUM ('Vulnerable', 'Weak', 'Moderate', 'Strong');
+ALTER TABLE "Password" ALTER COLUMN "strength" TYPE "PasswordStrength_new" USING ("strength"::text::"PasswordStrength_new");
+ALTER TYPE "PasswordStrength" RENAME TO "PasswordStrength_old";
+ALTER TYPE "PasswordStrength_new" RENAME TO "PasswordStrength";
+DROP TYPE "PasswordStrength_old";
+COMMIT;
