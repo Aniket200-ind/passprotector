7a8590741382f65f319449d9419812ec3f0b5e56
Aniket Botre <aniketbotre007@gmail.com>
2026-08-12 22:38:04 +0530

    feat: integrate PrismaPg adapter for improved database connection management

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,7 @@
 //! src/lib/prisma.ts
 
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 /**
  * ?Global Prisma client to prevent multiple instances in development.
@@ -8,6 +9,12 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL || "",
+});
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+    adapter,
+});
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
