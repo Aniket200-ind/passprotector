a363d88891686e8065c3e989ec2be148b87aee6a
Aniket Botre <aniketbotre007@gmail.com>
2026-08-20 22:32:35 +0530

    fix: simplify active tab logic and improve styling of ToggleGenerator component

--- src/components/features/ToggleGenerator.tsx
+++ src/components/features/ToggleGenerator.tsx
@@ -5,16 +5,12 @@
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { usePathname, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { useMemo } from "react";
 
 const ToggleGenerator = () => {
   const pathname = usePathname();
   const router = useRouter();
 
-  const activeTab = useMemo(
-    () => (pathname.includes("password") ? "password" : "passphrase"),
-    [pathname]
-  );
+  const activeTab = pathname.includes("password") ? "password" : "passphrase";
 
   return (
     <Tabs
@@ -22,17 +18,22 @@ const ToggleGenerator = () => {
       onValueChange={(value) =>
         router.push(`/generate/${value}`, { scroll: false })
       }
-      className="w-full max-w-sm my-8"
+      className="w-full max-w-sm mt-8 mb-4 flex justify-center"
       aria-label="Toggle between password and passphrase generator"
     >
-      <TabsList className="flex justify-center bg-charcoal p-1 rounded-lg sm:justify-start">
+      <TabsList
+        className={cn(
+          "rounded-lg",
+          "bg-golden/30 border border-golden",
+        )}
+      >
         <TabsTrigger
           value="password"
           className={cn(
-            "px-5 py-2 rounded-md transition-all duration-300 cursor-pointer",
+            "px-3 py-2 rounded-md transition-all duration-300 cursor-pointer",
             activeTab === "password"
               ? "text-cyberBlue bg-deepPurple shadow-cyberpunk"
-              : "text-gray-400 hover:text-cyberBlue"
+              : "text-white hover:text-cyberBlue",
           )}
           aria-label="Generate password"
         >
@@ -41,10 +42,10 @@ const ToggleGenerator = () => {
         <TabsTrigger
           value="passphrase"
           className={cn(
-            "px-5 py-2 rounded-md transition-all duration-300 cursor-pointer",
+            "px-3 py-2 rounded-md transition-all duration-300 cursor-pointer",
             activeTab === "passphrase"
               ? "text-synthwavePink bg-deepPurple shadow-cyberpunk"
-              : "text-gray-400 hover:text-synthwavePink"
+              : "text-white hover:text-synthwavePink",
           )}
           aria-label="Generate passphrase"
         >
