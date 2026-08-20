5c541acd935037e2cf55f4bd4d1e083e033df36e
Aniket Botre <aniketbotre007@gmail.com>
2026-08-20 22:32:12 +0530

    fix: enhance layout and styling of password generator page

--- src/app/(generation)/generate/password/page.tsx
+++ src/app/(generation)/generate/password/page.tsx
@@ -6,20 +6,21 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Password Generator | PassProtector",
-  description: "Generate strong, secure passwords with our customizable password generator tool."
+  description:
+    "Generate strong, secure passwords with our customizable password generator tool.",
 };
 
 export default function PasswordGeneratorPage() {
   return (
-    <main className="mx-auto py-12 px-4">
+    <main className="py-12 px-4 flex flex-col items-center">
       <ToggleGenerator />
-      <h1 className="animate-fade-in-up text-3xl motion-reduce:animate-none sm:text-4xl font-bold text-golden font-fancy text-center mb-4">
+      <h1 className="animate-fade-in-up text-3xl text-center motion-reduce:animate-none sm:text-4xl font-bold text-golden font-fancy mb-4">
         Generate Secure Passwords
       </h1>
-      <p className="text-center text-muted-foreground mb-8 max-w-lg mx-auto animate-fade-in-up animation-delay-100 motion-reduce:animate-none">
+      <p className="text-muted-foreground text-center mb-8 max-w-lg animate-fade-in-up animation-delay-100 motion-reduce:animate-none">
         Create strong, unique passwords with our customizable generator tool.
       </p>
-      <div className="max-w-3xl mx-auto">
+      <div className="w-full max-w-md">
         <PasswordGenerator />
       </div>
     </main>
